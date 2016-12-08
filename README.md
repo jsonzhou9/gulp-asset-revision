00931e7cf91f5bd2fe76c249f31589b9f45b869b
Jason <zzyss86@qq.com>
2016-12-08 21:49:23 +0800

    v1.0

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Revises the version of the resource referenced in the html.
 
-- Alose via: [gulp-asset-revision](https://github.com/zzyss86/gulp-hash-list)
+- Works with: [gulp-hash-list](https://github.com/zzyss86/gulp-hash-list)
 - Author: JsonZhou
 - Author Blog: [http://www.2fz1.com/](http://www.2fz1.com/)
 
@@ -9,8 +9,43 @@ Revises the version of the resource referenced in the html.
 	npm install --save-dev gulp-asset-revision
 	
 ## Usage
+
+	var gulp = require('gulp');
+	var hash = require('gulp-hash-list');
+	var revision = require('gulp-asset-revision');
+	
+	gulp.task('hash', function() {
+	    return gulp.src(['./src/**/*.js','./src/**/*.css'])
+	        .pipe(hash({
+	            "template": "{name}{ext}?hash={hash}"
+	        }))
+	        .pipe(gulp.dest('./dist'))
+	        .pipe(hash.manifest('assets.json'))
+	        .pipe(gulp.dest('./manifest'));
+	});
+	
+	gulp.task('revision', ['hash'], function() {
+	    return gulp.src(['./manifest/assets.json','./pages/*.html'])
+	        .pipe(revision({
+	            hasSuffix: false,
+	            manifest: './manifest/assets.json'
+	        }))
+	        .pipe(gulp.dest('./pages/'));
+	});
 	
 ## API
 
-## 重复造轮子的原因
+### revision(options)
+
+|option|default|description|
+|---|---|---|
+|manifest||**Required**, asset manifest file path|
+|hasSuffix|false|If true,path matching will add `pathSuffix` |
+|pathSuffix|-[0-9a-f]{8,10}|Support regular|
+
+## hasSuffix和pathSuffix的说明
+
+当路径不是`{name}{ext}?param=value`形式时，`gulp-asset-revision`在扫描文件引用地址时，无法正确匹配地址，比如：`{name}-{hash}{ext}`
+
+
 
--- index.js
+++ index.js
@@ -1,1 +1,87 @@
+var gutil = require('gulp-util'),
+    PluginError = gutil.PluginError,
+    assign = require('lodash.assign'),
+    through2 = require('through2'),
+    path = require('path');
 
+var PluginName = 'gulp-asset-revision';
+
+function getManifestData(manifest){
+    var manifestData = require(manifest);
+    if(manifestData){
+        return manifestData;
+    }else{
+        return null;
+    }
+}
+
+/**
+ * Escape character：- [ ] { } ( ) * + ? . ^ $ | / \
+ * Add \
+ * "{}" => "\{\}"
+ * */
+function escPathPattern(pattern) {
+    return pattern.replace(/[\-\[\]\{\}\(\)\*\+\?\.\^\$\|\/\\]/g, "\\$&");
+}
+
+function closeDirBySep(dirname) {
+    return dirname + (!dirname || new RegExp( escPathPattern('/') + '$' ).test(dirname) ? '' : '/');
+}
+
+/**
+ * Main function
+ * */
+function revision(options){
+    options = assign({}, {
+        pathSuffix: '-[0-9a-f]{8,10}'
+    }, options);
+
+    if(!options.manifest){
+        return through2.obj(function(file,encoding,done){
+            done(new PluginError(PluginName, 'options mast set manifest'),file);
+        });
+    }else if(!options.manifest.endsWith('.json')){
+        return through2.obj(function(file,encoding,done){
+            done(new PluginError(PluginName, 'manifest must be JSON file'),file);
+        });
+    }
+
+    var manifest = getManifestData(path.join(process.cwd(),options.manifest));
+
+    return through2.obj(function(file, encoding, done) {
+        if (file.isDirectory() || file.isNull() || !manifest) {
+            done(null, file);
+            return;
+        }
+
+        var changes = [];
+
+        for (var originalPath in manifest) {
+            var patterns = [escPathPattern(originalPath)];
+            var hashPath = manifest[originalPath];
+            if(options.hasSuffix){
+                patterns.push( escPathPattern( (path.dirname(originalPath) === '.' ? '' : closeDirBySep(path.dirname(originalPath)) ) + path.basename(originalPath, path.extname(originalPath)) )
+                    + options.pathSuffix
+                    + escPathPattern( path.extname(originalPath) )
+                );
+            }
+
+            patterns.forEach(function(pattern){
+                changes.push({
+                    regexp: new RegExp( '([\/\\\\\'"\(])' + pattern+'([\'\"\)])', 'g' ),
+                    replacement: '$1' + hashPath + '$2'
+                });
+            });
+        }
+
+        var src = file.contents.toString('utf8');
+        changes.forEach(function (changeItem) {
+            src = src.replace(changeItem.regexp, changeItem.replacement);
+        });
+
+        file.contents = new Buffer(src);
+        done(null,file);
+    });
+}
+
+module.exports = revision;
